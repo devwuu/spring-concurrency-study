b4eeed8b908331ad575515f4b37c4ed68da648cd
devwuu <jdw0477@gmail.com>
2024-01-04 17:21:42 +0900

    Redisson VS Lettuce Mysql VS Redis

--- src/main/java/com/example/springconcurrencystudy/repository/RedisLockRepository.java
+++ src/main/java/com/example/springconcurrencystudy/repository/RedisLockRepository.java
@@ -9,8 +9,29 @@ import java.time.Duration;
 @Component
 public class RedisLockRepository{
 
-    // redis를 사용하게 되면 db의 named lock을 직접 사용할 때보다
-    // session 관리가 용이하다
+    // redis를 사용했을 때 장단점
+    // db의 named lock을 직접 사용할 때보다 session 관리가 용이하다
+    // 활용중인 redis가 없다면 별도의 구축 비용과 관리 비용이 발생한다
+    // mysql보다 성능이 좋다 => 더 많은 요청 처리가 가능하다
+
+    // mysql을 사용했을 때 장단점
+    // 이미 mysql을 사용하고 있다면 별도의 비용 없이 사용 가능하다
+    // 어느 정도의 트래픽까지는 문제없이 활용 가능하다
+    // redis보다는 성능이 좋지 않다
+
+    // 따라서 실무에서는 많은 요청을 필요로 하거나 이미 활용중인 redis가 있을 때 redis를 사용한다.
+    // 비용적 여유가 없거나 요청이 충분히 많지 않은 경우엔 mysql을 사용한다.
+
+    // Lettuce 의 장단점
+    // 구현이 간단하고 Spring data redis를 사용하면 Lettuce가 기본이기 때문에 별도의 라이브러리를 사용하지 않아도 된다
+    // 하지만 spin lock 방식이기 때문에 동시에 많은 스레드가 lock을 획득하기 위해 대기중이라면 redis에 부하가 갈 수 있다 (반복해서 시도하기 때문)
+
+    // Redisson의 장단점
+    // 락 획득 재시도를 기본으로 제공한다.
+    // pub-sub 방식으로 구현되어 있기 때문에 Lettuce에 비해 redis에 부하가 덜 된다
+    // 하지만 별도의 라이브러리를 사용해야 하고 lock을 라이브러리 차원에서 제공해주기 때문에 사용법을 공부해야 한다.
+
+    // 실무에서는 재시도가 필요하지 않는 lock 은 Lettuce를, 재시도가 필요한 lock은 redisson을 사용한다
 
     private RedisTemplate<String, String> redisTemplate;
 
